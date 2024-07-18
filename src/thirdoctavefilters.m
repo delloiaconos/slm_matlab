5e3b26578664bf47e00dbf98e6e76ef13b2d3d08
Salvatore Dello Iacono <delloiaconos@gmail.com>
2024-07-18 22:25:42 +0200

    Added support to variable argument input.

--- src/thirdoctavefilters.m
+++ src/thirdoctavefilters.m
@@ -1,13 +1,29 @@
-function [ thirdOctaveFilterBank, centralFrequencies ] = thirdoctavefilters()
+function [ thirdOctaveFilterBank, centralFrequencies ] = thirdoctavefilters( varargin )
     % This function returns a bank of third octave filters according to 
     % ANSI S1.11-2004
 
-    % Third octave band filter design
+    % Local Fields
+    locVars = [ "bandsPerOctave", "filterOrder", "centralF", "fs" ];
 
+    % Default Settings
     bandsPerOctave = 3;
     filterOrder = 6;
     centralF = 1000;
     fs = 48000;
+
+    % Check if any field is present
+    vkindex = [];
+    for idx=1:length(locVars)
+        var = locVars(idx);
+        loc =  find( cellfun(@(v) ( isstring(v) || ischar(v) ) && strcmpi( var, v ), varargin ) );
+        if ~isempty(loc) && ~ismember(loc, vkindex) && (loc <= nargin-1) 
+            eval( fprintf( "%s = varargin{loc+1};", var ) );
+            vkindex = [vkindex loc loc+1];
+        end
+    end
+
+    % Third octave band filter design
+
     thirdOctaveFilter = fdesign.octave(bandsPerOctave, 'Class 0', 'N,F0', ...
         filterOrder, centralF, fs);
 
